57de5edb767fd0b9a1ce2327267b964a0e8f3038
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-04-30 08:07:18 +1000

    format of "date" is altered because it caused an issue when drawing horizontal lines on error bars

--- R/Analysis.R
+++ R/Analysis.R
@@ -19,11 +19,13 @@ source("R//functions.R")
 ##################
 #Process dataste #
 ##################
-extr <- read.csv("Data//WTC_extractable.csv", colClasses = c("time" = "factor", "location" = "factor"))
+extr <- read.csv("Data//WTC_extractable.csv", colClasses = c("time" = "factor", "location" = "factor", "date" = "character"))
 extr <- extr[complete.cases(extr), ]
 extr <- droplevels(extr)
 extr$chamber <- factor(ifelse(extr$chamber < 10, paste("0", extr$chamber, sep = ""), extr$chamber))
-extr$date <- dmy(extr$date)
+extr$date <- as.Date(dmy(extr$date)) 
+  #dmy makes variables POSIXct but horizontal line of errobars won't be plotted proparly probably
+  # because of difference scales
 extr$id <- extr$chamber:extr$side
 save(extr, file = "Output//Data/WTC_ExtractableNutrient.RData")
 
