88f0df8c6ed4dd49aefd0937b6dfb68bc406d7eb
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-05-08 13:59:52 +1000

    since box-cox lambda is used, power(1/3) try is removed

--- R/Stats_NH.R
+++ R/Stats_NH.R
@@ -40,12 +40,3 @@ qqnorm(Fml, ~ resid(.)|chamber)
 qqnorm(residuals.lm(Fml))
 qqline(residuals.lm(Fml))
 # not very great, but not too terrible...(?)
-
-### power(1/3) transformation ###
-bxplts(value = "nh", ofst= 0.29, data = extr)
-m1 <- lme((nh + .29)^(1/3) ~ temp * time, random = ~1|chamber/side, data = extr)
-plot(m1)
-qqnorm(m1, ~ resid(.)|chamber)
-qqnorm(residuals.lm(m1))
-qqline(residuals.lm(m1))
-# not really different than log transformation so stay with log
